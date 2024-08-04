dc68b83fc4b5d1153c22116213c34a4d7c22d631
Malysh7779 <aumalyshev@yandex.ru>
2024-08-04 13:49:19 +0500

    introduction UI

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,34 @@
 <template>
-    <p>Hello home</p>
+    <div class="md-body">
+        <h1>Hello home</h1>
+        <p>Edit me</p>
+        <div class="card__wrapper" v-for="item in items" :key="item.id">
+            <Card :name="`${item.lvl} lvl`" :title="item.title"/>
+        </div>
+    </div>
 </template>
+
+<script>
+import Card from '@/components/UI/Card'
+export default {
+    components: { Card },
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    title: 'Archer',
+                    descr: 'archer archer archer archer',
+                    lvl: 4
+                },
+                {
+                    id: 2,
+                    title: 'Wizard',
+                    descr: 'Wizard Wizard Wizard Wizard',
+                    lvl: 5
+                },
+            ]
+        }
+    }
+}
+</script>
